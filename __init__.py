e18bccec993a1b62f115556702a70c9dc0535f6d
kpsss34 <pum.otop@gmail.com>
2025-07-28 16:25:19 +0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,4 +2,4 @@ from .SD35Plus_node import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
 
-print("✅ Loaded: SD3.5 Plus Custom Nodes (Loader & Enhancer)")
+print("Loaded: SD3.5 Plus Custom Nodes (Loader & Enhancer)")
